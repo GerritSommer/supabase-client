11d1c101a07dccb62b8a041db0bdd6dda82b5ab3
Gerrit Sommer <gerrit.sommer@outlook.com>
2021-11-10 13:41:39 +0100

    use pretender for tests

--- tests/unit/adapters/file-test.js
+++ tests/unit/adapters/file-test.js
@@ -1,6 +1,6 @@
-import { module, test }      from 'qunit';
-import { setupTest }         from 'ember-qunit';
-import { rest, setupWorker } from 'msw';
+import { module, test } from 'qunit';
+import { setupTest }    from 'ember-qunit';
+import Pretender        from 'pretender';
 
 module('Unit | Adapter | file', function(hooks) {
   setupTest(hooks);
@@ -14,9 +14,10 @@ module('Unit | Adapter | file', function(hooks) {
   });
 
   test('a query has all needed parameters', async function(assert) {
-    assert.expect(3);
-    const store = this.owner.lookup('service:store');
-    const body = Object.freeze({
+    assert.expect(2);
+    const server = new Pretender();
+    const store  = this.owner.lookup('service:store');
+    const body   = Object.freeze({
       "limit": 100,
       "offset": 0,
       "sortBy": {
@@ -26,19 +27,15 @@ module('Unit | Adapter | file', function(hooks) {
       "prefix": ""
     });
 
-    const worker = setupWorker(
-      rest.post('https://test.supabase.co/storage/v1/object/list/public', (request, response, ctx)=> {
-        assert.deepEqual(request.body, body, 'all parameters present in the request body');
-        assert.equal(request.url.origin, 'https://test.supabase.co', 'the correct host is used');
-        assert.equal(request.url.pathname, '/storage/v1/object/list/public', 'the path is builded');
+    server.post('https://test.supabase.co/storage/v1/object/list/public', (request) => {
+      const requestBody = JSON.parse(request.requestBody);
 
-        return response(
-          ctx.status(200),
-          ctx.json([])
-        );
-      })
-    );
-    await worker.start();
+      assert.deepEqual(requestBody, body, 'all parameters present in the request body');
+      assert.equal(request.url, 'https://test.supabase.co/storage/v1/object/list/public', 'the correct host is used');
+
+      return [200, {"Content-Type": "application/json"}, JSON.stringify([])]
+
+    });
 
     await store.query('file', { path: 'public' });
   });
